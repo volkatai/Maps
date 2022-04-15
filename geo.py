e2456f1b098e37581a02309c1463a48a138a9943
User <volkatai@yandex.ru>
2022-04-15 17:17:04 +0300

    ...

--- geo.py
+++ geo.py
@@ -1,56 +1,57 @@
+import math
+
 import requests
 from math import sin, cos, sqrt, atan2, radians
 
 
-def get_coordinates(city):
-    url = "https://geocode-maps.yandex.ru/1.x/"
-
-    params = {
-        'geocode': city,
-        'format': 'json',
-        'apikey': "40d1649f-0493-4b70-98ba-98533de7710b"
-    }
-
-    response = requests.get(url, params)
-    json = response.json()
-    point_str = json['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
-    point_array = [float(x) for x in point_str.split(' ')]
-
-    return point_array
-
-
-def get_country(city):
-    url = "https://geocode-maps.yandex.ru/1.x/"
-
-    params = {
-        'geocode': city,
-        'format': 'json',
-        'apikey': "40d1649f-0493-4b70-98ba-98533de7710b"
-    }
-
-    response = requests.get(url, params)
-    json = response.json()
-
-    return \
-        json['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty'][
-            'GeocoderMetaData'][
-            'AddressDetails']['Country']['CountryName']
+def get_geo_info(city_name, type_info):
+    try:
+        # url, по которому доступно API Яндекс.Карт
+        url = "https://geocode-maps.yandex.ru/1.x/"
+        # параметры запроса
+        params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            # город, координаты которого мы ищем
+            'geocode': city_name,
+            # формат ответа от сервера, в данном случае JSON
+            'format': 'json'
+        }
+        # отправляем запрос
+        response = requests.get(url, params)
+        # получаем JSON ответа
+        json = response.json()
+        # получаем координаты города
+        # (там написаны долгота(longitude), широта(latitude) через пробел)
+        # посмотреть подробное описание JSON-ответа можно
+        # в документации по адресу https://tech.yandex.ru/maps/geocoder/
+        if type_info == 'coordinates':
+            coordinates_str = json['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+            # Превращаем string в список, так как
+            # точка - это пара двух чисел - координат
+            long, lat = map(float, coordinates_str.split())
+            # Вернем ответ
+            return long, lat
+        elif type_info == 'country':
+            return json['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty'][
+                'GeocoderMetaData']['AddressDetails']['Country']['CountryName']
+    except Exception as e:
+        return e
 
 
 def get_distance(p1, p2):
-    R = 6373.0
+    # p1 и p2 - это кортежи из двух элементов - координаты точек
+    radius = 6373.0
 
-    lon1 = radians(p1[0])
-    lat1 = radians(p1[1])
-    lon2 = radians(p2[0])
-    lat2 = radians(p2[1])
+    lon1 = math.radians(p1[0])
+    lat1 = math.radians(p1[1])
+    lon2 = math.radians(p2[0])
+    lat2 = math.radians(p2[1])
 
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
+    d_lon = lon2 - lon1
+    d_lat = lat2 - lat1
 
-    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    a = math.sin(d_lat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(d_lon / 2) ** 2
+    c = 2 * math.atan2(a ** 0.5, (1 - a) ** 0.5)
 
-    distance = R * c
-
+    distance = radius * c
     return distance
