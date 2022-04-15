40e01dedd1319f10fd642288a6e330e0ec97587c
User <volkatai@yandex.ru>
2022-04-15 17:23:54 +0300

    .....

---
+++ Alice_and_maps.py
@@ -0,0 +1,59 @@
+from flask import Flask, request
+import logging
+import json
+# импортируем функции из нашего второго файла geo
+from geo import get_geo_info, get_distance
+
+app = Flask(__name__)
+
+# Добавляем логирование в файл. Чтобы найти файл,
+# перейдите на pythonwhere в раздел files, он лежит в корневой папке
+logging.basicConfig(level=logging.INFO, filename='app.log',
+                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
+
+
+@app.route('/post', methods=['POST'])
+def main():
+    logging.info('Request: %r', request.json)
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+    handle_dialog(response, request.json)
+    logging.info('Request: %r', response)
+    return json.dumps(response)
+
+
+def handle_dialog(res, req):
+    user_id = req['session']['user_id']
+    if req['session']['new']:
+        res['response']['text'] = 'Привет! Я могу сказать в какой стране город или сказать расстояние между городами!'
+        return
+    # Получаем города из нашего
+    cities = get_cities(req)
+    if not cities:
+        res['response']['text'] = 'Вы не написали название ни одного города!'
+    elif len(cities) == 1:
+        res['response']['text'] = 'Этот город в стране - ' + get_geo_info(cities[0], 'country')
+    elif len(cities) == 2:
+        distance = get_distance(get_geo_info(cities[0], 'coordinates'), get_geo_info(cities[1], 'coordinates'))
+        res['response']['text'] = 'Расстояние между этими городами: ' + str(round(distance)) + ' км.'
+        res['response']['text'] = 'Расстояние между этими городами: ' + str(round(distance)) + ' км.'
+    else:
+        res['response']['text'] = 'Слишком много городов!'
+
+
+def get_cities(req):
+    cities = []
+    for entity in req['request']['nlu']['entities']:
+        if entity['type'] == 'YANDEX.GEO':
+            if 'city' in entity['value']:
+                cities.append(entity['value']['city'])
+    return cities
+
+
+if __name__ == '__main__':
+    app.run()
